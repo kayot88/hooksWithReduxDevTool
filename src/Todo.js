efd93998cbcaf46cd0575fc2c8bbbc76c76796ce
Kayot88 <kayot88@gmail.com>
2021-02-24 22:55:03 +0200

    useState+useReducer+reduxDevTools

---
+++ src/Todo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { actions } from "./App";
+const Todo = ({ todo, dispatch }) => {
+  return (
+    <div>
+      <span style={{ color: todo.complete ? "#AAA" : "#000" }}>
+        {todo.name}
+      </span>
+      <button
+        onClick={() => {
+          dispatch({ type: actions.COMPLETE_TODO, payload: { id: todo.id } });
+          console.log("todo.id", todo.id);
+        }}
+      >
+        Complete
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: actions.REMOVE_TODO, payload: { id: todo.id } });
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default Todo;
